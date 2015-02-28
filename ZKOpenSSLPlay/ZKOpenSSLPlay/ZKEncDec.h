8b566588e3548f2f210d31cc3180576413f38ea7
Zeeshan Khan <zeeshankhan@Zeeshans-MacBook-Pro.local>
2015-02-28 11:47:03 +0530

    Added resource links

--- ZKOpenSSLPlay/ZKOpenSSLPlay/ZKEncDec.h
+++ ZKOpenSSLPlay/ZKOpenSSLPlay/ZKEncDec.h
@@ -8,6 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
+// Content across the web about Encryption Decryption technologies
+// https://github.com/saju/misc/tree/master/misc
+
+
 //unsigned char *encryptData(unsigned char *key_data, char *data, int *len) ;
 //char *decryptData(unsigned char *key_data, unsigned char *ciphertext, int *clen);
 
